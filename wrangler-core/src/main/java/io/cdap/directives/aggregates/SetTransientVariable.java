d62c66a5b18d2051501c98ca53f2ec613e4d2e19
SARATHI M <sarathimkanagavalli@gmail.com>
2025-04-15 17:06:57 +0530

    Update SetTransientVariable.java

--- wrangler-core/src/main/java/io/cdap/directives/aggregates/SetTransientVariable.java
+++ wrangler-core/src/main/java/io/cdap/directives/aggregates/SetTransientVariable.java
@@ -67,7 +67,7 @@ public class SetTransientVariable implements Directive {
 
   @Override
   public void initialize(Arguments args) throws DirectiveParseException {
-    this.variable = ((Identifier) args.value("variable", "MB")).value();
+ this.variable = ((Identifier) args.value("variable", "MB")).value();
     String expression = ((Expression) args.value("condition", "MB")).value();
     try {
       el = EL.compile(expression);
